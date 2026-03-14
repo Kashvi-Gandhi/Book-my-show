0883552a99008f681eeb8a1f3ff1ce871f884d1c
Kashvi-Gandhi <kashvig2014@gmail.com>
2026-03-14 20:28:00 +0530

    Updated Django project

--- api/index.py
+++ api/index.py
@@ -1,4 +1,9 @@
 import os
+import sys
+
+# Add the project directory to the path
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'bookmyseat'))
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookmyseat.settings')
 
 from vercel_django import handler

ac0bc6e1651f12135c5dc4fcf9623e4cc51768c2
Kashvi-Gandhi <kashvig2014@gmail.com>
2026-03-14 20:17:15 +0530

    Updated Django project

--- bookmyseat/bookmyseat/settings.py
+++ bookmyseat/bookmyseat/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-uzhccn3tx)q4f*-h(=pn**mxt6t)ckky&k!j^6mk92lwyxuapg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["book-my-show-blue.vercel.app"]
 
 
 # Application definition

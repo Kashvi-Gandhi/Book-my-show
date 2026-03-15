f678778b975a27ac7782116078f766db3be58f73
Kashvi-Gandhi <kashvig2014@gmail.com>
2026-03-15 10:47:52 +0530

    Fixed settings.py

--- bookmyseat/bookmyseat/bookmyseat/settings.py
+++ bookmyseat/bookmyseat/bookmyseat/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*",".vercel.app"]
+ALLOWED_HOSTS = ['.vercel.app', 'localhost', '127.0.0.1']
 
 
 # Application definition
